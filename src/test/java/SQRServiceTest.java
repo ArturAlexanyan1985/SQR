d79a67f117d6aea6e528481b50449a5bb7f03be2
ArturAlexanyan1985 <artur_aleksanyan@inbox.ru>
2022-08-25 18:03:57 +0300

    First commit

--- src/test/java/SQRServiceTest.java
+++ src/test/java/SQRServiceTest.java
@@ -1,15 +1,12 @@
-package ru.netology.sqr;
-
-import org.testng.annotations.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 public class SQRServiceTest {
     @Test
     public void test0() {
         ru.netology.sqr.SQRService service = new ru.netology.sqr.SQRService();
         int actual = service.calculate(300, 200);
-        int expected = 3;
+        int expected = 0;
 
         assertEquals(expected, actual);
     }
@@ -18,7 +15,7 @@ public class SQRServiceTest {
     public void test1() {
         ru.netology.sqr.SQRService service = new ru.netology.sqr.SQRService();
         int actual = service.calculate(0, 300);
-        int expected = 3;
+        int expected = 8;
 
         assertEquals(expected, actual);
     }
@@ -27,7 +24,7 @@ public class SQRServiceTest {
     public void test2() {
         ru.netology.sqr.SQRService service = new ru.netology.sqr.SQRService();
         int actual = service.calculate(0, 3);
-        int expected = 3;
+        int expected = 0;
 
         assertEquals(expected, actual);
     }
@@ -36,7 +33,7 @@ public class SQRServiceTest {
     public void test3() {
         ru.netology.sqr.SQRService service = new ru.netology.sqr.SQRService();
         int actual = service.calculate(0, 10_000);
-        int expected = 3;
+        int expected = 90;
 
         assertEquals(expected, actual);
     }
